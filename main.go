6796ffc0a963429a3e1844b89f7a2ede32ee694e
Swayam Duhan <workplace.swayam@gmail.com>
2025-02-21 10:50:31 +0530

    added color sequence

--- main.go
+++ main.go
@@ -10,6 +10,15 @@ import (
 	"github.com/swayamduhan/shell-go/builtins"
 )
 
+const (
+	// ANSI color codes
+	Reset  = "\033[0m"
+	Green  = "\033[32m"
+	Bold   = "\033[1m"
+	Cyan   = "\033[36m"
+	Yellow = "\033[33m"
+)
+
 
 
 func handleCmd(cmd string){
@@ -18,13 +27,14 @@ func handleCmd(cmd string){
 	switch tokens[0] {
 	case "echo":
 		builtins.HandleEcho(tokens)
-
 	case "exit":
 		builtins.HandleExit(tokens)
 	case "type":
 		builtins.HandleType(tokens)
 	case "pwd":
 		builtins.HandleWorkingDir()
+	case "cd":
+		builtins.HandleChangeDir(tokens)
 	case "":
 		fmt.Println("enter a command")
 	default: 
@@ -34,8 +44,10 @@ func handleCmd(cmd string){
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
+
 	for {
-		fmt.Printf("$> ")
+		path, _ := os.Getwd()
+		fmt.Printf("%s%s$%s %s%s%s> ", Bold, Green, Reset, Cyan, path, Reset)
 		cmd, err := reader.ReadString('\n')
 		if err != nil {
 			log.Println("error reading command: ", err)
@@ -45,4 +57,5 @@ func main() {
 		cmd = strings.TrimSpace(cmd)
 		handleCmd(cmd)
 	}
+	
 }
